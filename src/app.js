a9ea2c5a50129c3a7f2aaddc4221951b6bc29b23
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2021-04-18 19:22:11 +0700

    CRA Starter

--- src/app.js
+++ src/app.js
@@ -14,9 +14,16 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '..', 'public')));
+if (app.get('env') !== 'production') app.use(express.static(path.join(__dirname, '..', 'public')));
+if (app.get('env') === 'production')
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 app.use('/', router);
+
+if (app.get('env') === 'production')
+  app.get('*', (req, res) => {
+    return res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
