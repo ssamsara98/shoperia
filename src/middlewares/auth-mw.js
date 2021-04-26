e5e64873d1c08ca2566e5b126d27ecb0f5e57a4d
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2021-04-26 09:04:43 +0700

    Fix authentication error

--- src/middlewares/auth-mw.js
+++ src/middlewares/auth-mw.js
@@ -1,9 +1,13 @@
+const createHttpError = require('http-errors');
 const passport = require('passport');
 
 const authMw = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err) {
       return next(err);
+    }
+    if (!user) {
+      return next(createHttpError(401, 'Email or Password is incorrect'));
     }
     req.user = user;
     return next();
