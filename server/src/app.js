0c356f2f221b13e1bc74bec4b779927d94eb2caf
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2021-07-12 22:58:49 +0700

    Enable trust proxy

--- server/src/app.js
+++ server/src/app.js
@@ -14,6 +14,8 @@ const app = express();
 app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'ejs');
 
+app.enable('trust proxy');
+
 require('./utils/passport-helper');
 app.use(compression());
 app.use(morgan('dev'));
